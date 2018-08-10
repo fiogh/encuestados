d38d790f8f8c9e93d9e3e7e440d255ca1a8cbde4
Fiorella Miguez <fmiguez@Produccion-48.DOMSECIU>
2018-08-10 13:44:47 -0300

    Agregué el módulo storage.

---
+++ recursos-encuestados/js/modelo/storage.js
@@ -0,0 +1,19 @@
+/*
+ * Storage
+ */
+var Storage = function() {
+  this.myJSON = [];
+};
+
+Storage.prototype = {
+  //se almacena objeto
+  almacenarObjeto: function(objeto) {
+    this.myJSON.push({"idpregunta": objeto.id, "pregunta": objeto.pregunta});
+    localStorage.setItem('preguntas', JSON.stringify(myJSON));
+  },
+
+  seObtieneObjeto: function() {
+    var retrievedObject = localStorage.getItem('testObject');
+    return JSON.parse(retrievedObject);
+  },
+};
